9f48b5e6078fbe908cca2fefd155949348537e10
FADY ADIB <fadeehab36@gmail.com>
2026-05-18 01:50:03 +0300

    solve erorr

--- frontend/blood-donation-frontend/src/environments/environment.development.ts
+++ frontend/blood-donation-frontend/src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
   // baseurl: 'http://localhost:5000',
   // apiUrl: 'http://localhost:5000/api'
 
-  baseUrl: 'https://blood-donation-system-58db.vercel.app',
+  baseurl: 'https://blood-donation-system-58db.vercel.app',
   apiUrl: 'https://blood-donation-system-58db.vercel.app/api'
 };
